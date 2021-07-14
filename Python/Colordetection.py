88b6729d3eb613a156fae7e427b6597d68fe2f1a
Asbjørn Stokka <asbjorn.stokka@lyse.net>
2021-07-14 20:40:35 +0200

    Python, detect/mask color. Red mask is ok.

---
+++ Python/Colordetection.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+import numpy as np
+import cv2
+# construct the argument parse and parse the arguments
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+# define boundaries (lower)
+bound_red = np.array([110,100,139])
+
+upper = np.array([180,255,255])
+
+running = 0
+while(running < 4):
+    # Capture the video frame by frame
+    ret, image = vid.read()
+    
+    # loop over the boundaries
+    # for (lower, upper) in boundaries:
+        # create NumPy arrays from the boundaries
+    # lower = np.array(lower, dtype = "uint8")
+    # upper = np.array(upper, dtype = "uint8")
+        # find the colors within the specified boundaries and apply
+        # the mask
+    hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, bound_red, upper)
+    output = cv2.bitwise_and(image, image, mask = mask)
+        # show the images
+    cv2.imshow("images", np.hstack([image, output]))
+        # cv2.waitKey(0)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        running += 1
+
+
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+

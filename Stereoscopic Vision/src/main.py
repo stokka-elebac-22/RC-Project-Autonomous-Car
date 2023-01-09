e77f4bd6f260910a37bc8b14d20af59e4a6cd93e
eirikreiestad <eirik@reiestad.no>
2023-01-09 14:54:37 +0100

    added new folder, steroescopic Vision

--- Stereoscopic Vision/src/main.py
+++ Stereoscopic Vision/src/main.py
@@ -1,4 +1,3 @@
-from qr_code import QRCode
 from camera import Camera
 
 # ----- ORIGINAL MEASUREMENTS -----
@@ -8,6 +7,5 @@ QR_SIZE_MM = 52
 QR_DISTANCE = 500 
 
 if "__main__" == __name__:
-    qr_code = QRCode(QR_SIZE_PX, QR_SIZE_MM, QR_DISTANCE) 
-    camera = Camera(qr_code) 
+    camera = Camera() 
     camera.run(verbose=2)

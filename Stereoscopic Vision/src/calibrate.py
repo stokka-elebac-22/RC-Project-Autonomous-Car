168ec2b4514f45cbd52f4d13ce0ad391e3f26a1b
eirikreiestad <eirik@reiestad.no>
2023-01-12 11:43:52 +0100

    changed the functions to calibrating two cameras at the same time (no option for only one anymore), and changed a bunch of variable names

--- Stereoscopic Vision/src/calibrate.py
+++ Stereoscopic Vision/src/calibrate.py
@@ -11,11 +11,17 @@ import glob
 
 CHECKERBOARD_DIMENSION = (8, 6)
 CRITERIA = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+DIRECTORY_LEFT = 'Stereoscopic Vision/images/calibrate_left/*.jpg'
+DIRECTORY_RIGHT = 'Stereoscopic Vision/images/calibrate_right/*.jpg'
 
 class Calibrate:
-    def __init__(self, criteria, checkerboard_dimension=(8, 6), directory=""):
+    def __init__(self, criteria, checkerboard_dimension=(8, 6), directory_left="", directory_right=""):
+        """
+        This code is for calibrating two cameras at the same time
+        """
         self.checkerboard_dimension = checkerboard_dimension
-        self.directory = directory
+        self.directory_left = directory_left
+        self.directory_right = directory_right
         self.criteria = criteria
 
         # prepare object points
@@ -25,33 +31,46 @@ class Calibrate:
     def calculate_object_and_image_points(self):
         # arrays to store object poitns and image points from all the images
         object_points = []
-        image_points = []
+        image_points_left = []
+        image_points_right = []
 
-        images = glob.glob(self.directory)
+        images_left = glob.glob(self.directory_left)
+        images_right= glob.glob(self.directory_right)
 
-        for name in images:
-            img = cv.imread(name)
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        images_returned_left = []
+        images_returned_right = []
+
+        for left, right in zip(images_left, images_right):
+            image_left = cv.imread(left)
+            image_right = cv.imread(right)
+            gray_left = cv.cvtColor(image_left, cv.COLOR_BGR2GRAY)
+            gray_right = cv.cvtColor(image_right, cv.COLOR_BGR2GRAY)
 
             # find the chess board corners
-            ret, corners = cv.findChessboardCorners(gray, self.checkerboard_dimension, None)
+            ret_left, corners_left = cv.findChessboardCorners(gray_left, self.checkerboard_dimension, None)
+            ret_right, corners_right = cv.findChessboardCorners(gray_right, self.checkerboard_dimension, None)
 
             # if found, add object point, image point (after refining them)
-            if ret:
+            if ret_left and ret_right:
                 object_points.append(self.object_point)
+                images_returned_left.append(left)
+                images_returned_right.append(right)
 
-                cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), self.criteria)
-                image_points.append(corners)
-
+                cv.cornerSubPix(gray_left, corners_left, (11, 11), (-1, -1), self.criteria)
+                image_points_left.append(corners_left)
+                cv.cornerSubPix(gray_right, corners_right, (11, 11), (-1, -1), self.criteria)
+                image_points_right.append(corners_right)
                 # draw and display the corners
-                cv.drawChessboardCorners(img, self.checkerboard_dimension, corners, ret)
-                # cv.imshow(name, img)
+                cv.drawChessboardCorners(image_left, self.checkerboard_dimension, corners_left, ret_left)
+                cv.drawChessboardCorners(image_right, self.checkerboard_dimension, corners_left, ret_left)
+                cv.imshow("left", image_left)
+                cv.imshow("right", image_right)
                 # cv.waitKey(0)
-        print(f"Could find chessboard corners in {len(object_points)} out of {len(images)} images")
+        print(f"Could find chessboard corners in {len(object_points)} out of {len(left)} images")
 
         cv.destroyAllWindows()
 
-        return object_points, image_points
+        return object_points, image_points_left, image_points_right, images_returned_left, images_returned_right
 
     def calibration(self, img, object_points, image_points):
         # calibrateCarmera returns the camera matrix, distortion coefficients, rotation and translation vectors
@@ -125,39 +144,42 @@ def stereo_rectify(new_mtx_left, dist_left, new_mtx_right, dist_right, img_left_
 if "__main__" == __name__:
 
     # Calibrate left camera
-    directory = 'Stereoscopic Vision/images/calibrate_left/*.jpg'
-    calibrate = Calibrate(CRITERIA, CHECKERBOARD_DIMENSION, directory)
-    images = glob.glob(directory) # get all images
-    img_left = cv.imread(images[0]) # just choosing the first one (assuming it did find the checkerboard on that one...)
-    gray_left = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY) # making the img gray
-    object_points, image_points_left = calibrate.calculate_object_and_image_points()
-    left_ret, left_mtx, left_dist, left_rvecs, left_tvecs = calibrate.calibration(gray_left, object_points, image_points_left)
-    left_new_camera_matrix, left_roi = calibrate.get_optimal_new_camera_matrix(img_left, left_mtx, left_dist)
-    # calibrate.undistort(img, left_mtx, left_dist, left_new_camera_matrix, left_roi)
+    calibrate = Calibrate(CRITERIA, CHECKERBOARD_DIMENSION, DIRECTORY_LEFT, DIRECTORY_RIGHT)
+    object_points, image_points_left, image_points_right, images_left, images_right = calibrate.calculate_object_and_image_points()
 
-    # Calibrate right camera
-    directory = 'Stereoscopic Vision/images/calibrate_right/*.jpg'
-    calibrate = Calibrate(CRITERIA, CHECKERBOARD_DIMENSION, directory)
-    images = glob.glob(directory) # get all images
-    img_right = cv.imread(images[0]) # just choosing the first one (assuming it did find the checkerboard on that one...)
-    gray_right = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY) # making the img gray
-    object_points, image_points_right = calibrate.calculate_object_and_image_points()
-    right_ret, right_mtx, right_dist, right_rvecs, right_tvecs = calibrate.calibration(img_right, object_points, image_points_right)
-    right_new_camera_matrix, right_roi = calibrate.get_optimal_new_camera_matrix(img_right, right_mtx, right_dist)
-    # calibrate.undistort(img, left_mtx, left_dist, left_new_camera_matrix, left_roi)
+    image_left = cv.imread(images_left[0])
+    gray_left = cv.cvtColor(image_left, cv.COLOR_BGR2GRAY)
+    image_right = cv.imread(images_left[0])
+    gray_right = cv.cvtColor(image_left, cv.COLOR_BGR2GRAY)
+
+    ret_left, mtx_left, left_dist, rvecs_left, tvecs_left = calibrate.calibration(gray_left, object_points, image_points_left)
+    ret_right, mtx_right, dist_right, rvecs_right, tvecs_right = calibrate.calibration(gray_right, object_points, image_points_right)
+
+    new_camera_matrix_left, roi_left = calibrate.get_optimal_new_camera_matrix(gray_left, mtx_left, left_dist)
+    new_camera_matrix_right, roi_right = calibrate.get_optimal_new_camera_matrix(gray_right, mtx_right, dist_right)
+    # calibrate.undistort(img, mtx_left, left_dist, new_camera_matrix_left, roi_left)
 
     # Stereo Calibration
-    retS, new_mtx_left, dist_left, new_mtx_right, dist_right, Rot, Trns, Emat, Fmat = stereo_calibrate(object_points, image_points_left, image_points_right, left_new_camera_matrix, left_dist, right_new_camera_matrix, right_dist, gray, CRITERIA)
+    retS, new_mtx_left, dist_left, new_mtx_right, dist_right, Rot, Trns, Emat, Fmat = stereo_calibrate(
+        object_points,
+        image_points_left,
+        image_points_right,
+        new_camera_matrix_left,
+        left_dist,
+        new_camera_matrix_right,
+        dist_right,
+        gray_left,
+        CRITERIA)
     # Stereo Rectification
-    rect_left, rect_right, proj_mat_left, proj_mat_right, Q, roi_left, roi_right, = stereo_rectify(new_mtx_left, left_dist, new_mtx_right, right_dist, gray_left, Rot, Trns)
-    left_stereo_map = cv.initUndistortRectifyMap(new_mtx_left, left_dist, rect_left, proj_mat_left, gray_left.shape[::-1], cv.CV_16SC2)
-    right_stereo_map = cv.initUndistortRectifyMap(new_mtx_right, right_dist, rect_right, proj_mat_right, gray_right.shape[::-1], cv.CV_16SC2)
+    rect_left, rect_right, proj_mat_left, proj_mat_right, Q, roi_left, roi_right, = stereo_rectify(new_mtx_left, left_dist, new_mtx_right, dist_right, gray_left, Rot, Trns)
+    stereo_map_left = cv.initUndistortRectifyMap(new_mtx_left, left_dist, rect_left, proj_mat_left, gray_left.shape[::-1], cv.CV_16SC2)
+    stereo_map_right = cv.initUndistortRectifyMap(new_mtx_right, dist_right, rect_right, proj_mat_right, gray_right.shape[::-1], cv.CV_16SC2)
 
     print("Saving parameters...")
     cv_file = cv.FileStorage("params.xml", cv.FILE_STORAGE_WRITE)
-    cv_file.write("left_stereo_map_x", left_stereo_map[0])
-    cv_file.write("left_stereo_map_y", left_stereo_map[1])
-    cv_file.write("right_stereo_map_x", right_stereo_map[0])
-    cv_file.write("right_stereo_map_y", right_stereo_map[1])
+    cv_file.write("stereo_map_left_x", stereo_map_left[0])
+    cv_file.write("stereo_map_left_y", stereo_map_left[1])
+    cv_file.write("stereo_map_right_x", stereo_map_right[0])
+    cv_file.write("stereo_map_right_y", stereo_map_right[1])
     cv_file.release()
 

c6cfde71e0a213e388632f38fc2c717bba4e7637
Asbjørn Stokka <asbjorn.stokka@lyse.net>
2023-01-13 15:55:43 +0100

    tests

--- computer_vision/qr_code/qr_code.py
+++ computer_vision/qr_code/qr_code.py
@@ -3,96 +3,25 @@ import dataclasses
 import cv2 as cv
 
 @dataclasses.dataclass
-class PointSet:   # Combine to QRGeometry?
+class QRGeometry:
     """PointSet, used by qrcode."""
     def __init__(self, pts=None):
         if pts is None or len(pts) < 4:
-            pts = [0,0,0,0]
-        self.points = pts
-        self.point0 = pts[0]
-        self.point1 = pts[1]
-        self.point2 = pts[2]
-        self.point3 = pts[3]
+            pts = [[0,0],[0,0],[0,0],[0,0]]
+        self.update(pts)
 
     def update(self, pts):
         """update points, used by qrcode."""
         self.points = pts
-        self.point0 = pts[0]
-        self.point1 = pts[1]
-        self.point2 = pts[2]
-        self.point3 = pts[3]
-
-@dataclasses.dataclass
-class SideSet:    # class names in singular
-    """SideSet, used by qrcode."""
-    def __init__(self, sides=None):
-        if sides is None or len(sides) < 4:
-            sides = [0,0,0,0]
-        self.side_a = sides[0]
-        self.side_b = sides[1]
-        self.side_c = sides[2]
-        self.side_d = sides[3]
-
-    def update(self, p_set: PointSet):
-        """update sides, used by qrcode."""
-        self.side_a = abs(p_set.point0[0] - p_set.point1[0])
-        self.side_b = abs(p_set.point1[1] - p_set.point2[1])
-        self.side_c = abs(p_set.point2[0] - p_set.point3[0])
-        self.side_d = abs(p_set.point3[1] - p_set.point0[1])
-class DisplayQRCode:
-    """DisplayQRCode, QR code placement estimation."""
-    def __init__(self):
-        ##### DISPLAY #####
-        self.color_frame_green = (0, 255, 0)
-        self.color_frame_red = (0, 0, 255)
-
-        self.font = cv.FONT_HERSHEY_SIMPLEX
-        self.font_scale = 0.5
-        self.text_color = (255, 0, 255)
-        self.text_thickness = 1
-
-    def display(self, frame, resize=1, verbose=1):
-        """Display """
-        for decoded_info, pts in zip(self.decoded_info, self.points.points):
-            if decoded_info:
-                color = self.color_frame_green
-            else:
-                color = self.color_frame_red
-            frame = cv.polylines(frame, [pts.astype(int)], True, color, 4)
-        if verbose > 0:
-            self.display_values(frame, verbose)
-        return frame
-
-    def display_values(self, frame, verbose=1):
-        """Display values"""
-        if verbose > 1:
-            text_location_a = (int(min(self.points.point0[0], self.points.point1[0]) + \
-                                    self.sides.side_a/2), int(self.points.point0[1]))
-            text_location_b = (int(self.points.point1[0]), int(min(self.points.point1[1], \
-                                    self.points.point2[1]) + self.sides.side_b/2))
-            text_location_c = (int(min(self.points.point2[0], self.points.point3[0]) + \
-                                    self.sides.side_c/2), int(self.points.point2[1]))
-            text_location_d = (int(self.points.point3[0]), int(min(self.points.point3[1], \
-                                    self.points.point0[1]) + self.sides.side_d/2))
-
-            cv.putText(frame, str(int(self.sides.side_a)), text_location_a, self.font, \
-                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
-            cv.putText(frame, str(int(self.sides.side_b)), text_location_b, self.font, \
-                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
-            cv.putText(frame, str(int(self.sides.side_c)), text_location_c, self.font, \
-                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
-            cv.putText(frame, str(int(self.sides.side_d)), text_location_d, self.font, \
-                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
-
-        frame = cv.putText(frame, f'angle    = {self.get_average_angle()}', (10, 20), self.font, \
-                            self.font_scale * 1.5, self.text_color, self.text_thickness, cv.LINE_AA)
-        frame = cv.putText(frame, f'distance = {self.get_average_distance()}', (10, 50), \
-                self.font, self.font_scale * 1.5, self.text_color, self.text_thickness, cv.LINE_AA)
+        self.side_a = abs(self.points[0][0] - self.points[1][0])
+        self.side_b = abs(self.points[1][1] - self.points[2][1])
+        self.side_c = abs(self.points[2][0] - self.points[3][0])
+        self.side_d = abs(self.points[3][1] - self.points[0][1])
 
 class QRCode:
     """QRCode, doing calculations for QR code placement estimation."""
-#                    c
-#            p2 ---------- p3
+#                          c
+#                  p2 ---------- p3
 #            |             |
 #         b  |             |  d
 #            |             |
@@ -103,9 +32,8 @@ class QRCode:
     def __init__(self, size_px, size_mm, distance, values_length=10):
         self.ret_qr = None
         self.decoded_info = None
-        self.points = PointSet()
+        self.qr_code = QRGeometry()
         self.rest = None
-        self.sides = SideSet()
 
         self.qr_size_px = size_px
         self.qr_size_mm = size_mm
@@ -120,13 +48,12 @@ class QRCode:
         """Update values """
         self.ret_qr = ret_qr
         self.decoded_info = decoded_info
-        self.points.update(points)
+        self.qr_code.update(points)
         self.rest = rest
-        self.sides.update(self.points)
-        width_px = max(abs(self.points.point0[0] - self.points.point1[0]) * (1 / resize),
-        abs(self.points.point2[0] - self.points.point3[0]))
-        height_px = max(abs(self.points.point3[1] - self.points.point0[1]),
-        abs(self.points.point2[1] - self.points.point1[1]))
+        width_px = max(abs(self.qr_code.points[0][0] - self.qr_code.points[1][0]) * (1 / resize),
+        abs(self.qr_code.points[2][0] - self.qr_code.points[2][0]))
+        height_px = max(abs(self.qr_code.points[2][1] - self.qr_code.points[0][1]),
+        abs(self.qr_code.points[2][1] - self.qr_code.points[1][1]))
 
         height_px_resize = height_px * (1/resize)
         ratio = width_px/height_px
@@ -159,6 +86,56 @@ class QRCode:
     def get_average_distance(self) -> int:
         """get averaged distance value """
         return sum(self.distance)//self.values_length
+
+class DisplayQRCode:
+    """DisplayQRCode, QR code placement estimation."""
+    def __init__(self):
+        ##### DISPLAY #####
+        self.color_frame_green = (0, 255, 0)
+        self.color_frame_red = (0, 0, 255)
+
+        self.font = cv.FONT_HERSHEY_SIMPLEX
+        self.font_scale = 0.5
+        self.text_color = (255, 0, 255)
+        self.text_thickness = 1
+
+    def display(self, frame, qrc: QRCode, qrg: QRGeometry, resize=1, verbose=1):
+        """Display """
+        for dec_info, pts in zip(qrc.decoded_info, qrc.qr_code.points):
+            if dec_info:
+                color = self.color_frame_green
+            else:
+                color = self.color_frame_red
+            frame = cv.polylines(frame, [pts.astype(int)], True, color, 4)
+        if verbose > 0:
+            self.display_values(frame, verbose, qrc, qrg)
+        return frame
+
+    def display_values(self, img, qrc: QRCode, qrg: QRGeometry, verbose=1):
+        """Display values"""
+        if verbose > 1:
+            text_location_a = (int(min(qrg.points[0][0], qrg.points[1][0]) + \
+                                    qrg.side_a/2), int(qrg.points[0][1]))
+            text_location_b = (int(qrg.points[1][0]), int(min(qrg.points[1][1], \
+                                    qrg.points[2][1]) + qrg.side_b/2))
+            text_location_c = (int(min(qrg.points[2][0], qrg.points[2][0]) + \
+                                    qrg.side_c/2), int(qrg.points[2][1]))
+            text_location_d = (int(qrg.points[2][0]), int(min(qrg.points[2][1], \
+                                    qrg.points[0][1]) + qrg.side_d/2))
+
+            cv.putText(img, str(int(qrg.side_a)), text_location_a, self.font, \
+                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
+            cv.putText(img, str(int(qrg.side_b)), text_location_b, self.font, \
+                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
+            cv.putText(img, str(int(qrg.side_c)), text_location_c, self.font, \
+                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
+            cv.putText(img, str(int(qrg.side_d)), text_location_d, self.font, \
+                        self.font_scale, self.text_color, self.text_thickness, cv.LINE_AA)
+
+        img = cv.putText(img, f'angle    = {qrc.get_average_angle()}', (10, 20), self.font, \
+                            self.font_scale * 1.5, self.text_color, self.text_thickness, cv.LINE_AA)
+        img = cv.putText(img, f'distance = {qrc.get_average_distance()}', (10, 50), \
+                self.font, self.font_scale * 1.5, self.text_color, self.text_thickness, cv.LINE_AA)
 
 def local_read_camera(name=None, resize=1):
     """Local read camera """
--- tests/test_qr_code.py
+++ tests/test_qr_code.py
@@ -1,25 +1,40 @@
 # test_qr_code.py
-from computer_vision.qr_code.qr_code import PointSet, SideSet
+from computer_vision.qr_code.qr_code import QRGeometry
 import pytest
 
-def test_points_update():
+def test_points_empty():
     """Testing points"""
-    points = PointSet()
-    points.update([0, 1, 2, 3])
-    assert points.point0 == 0
-    assert points.point1 == 1
-    assert points.point2 == 2
-    assert points.point3 == 3
+    qr_code = QRGeometry()
+    assert qr_code.points[0] == [0, 0] and qr_code.points[1] == [0, 0] and \
+            qr_code.points[2] == [0, 0] and qr_code.points[3] == [0, 0]
 
-def test_sides_update():
+@pytest.mark.parametrize(
+    ["pts", "exp"],
+    [
+        ([[0, 0], [0, 1], [1, 1], [1, 0]], [[0, 0], [0, 1], [1, 1], [1, 0]]),
+        ([[3, 1], [2, 4], [1, 4], [4, 1]], [[3, 1], [2, 4], [1, 4], [4, 1]]),
+        ([[4, 6], [7, 1], [1, 2], [4, 4]], [[4, 6], [7, 1], [1, 2], [4, 4]])
+    ]
+)
+def test_points_update(pts, exp):
+    """Testing update points"""
+    qr_code = QRGeometry(pts)
+    assert qr_code.points[0] == exp[0] and qr_code.points[1] == exp[1] and \
+            qr_code.points[2] == exp[2] and qr_code.points[3] == exp[3]
+
+@pytest.mark.parametrize(
+    ["pts", "exp"],
+    [
+        ([[0, 0], [1, 0], [1, 1], [0, 1]], [1, 1, 1, 1]),
+        ([[2,-2], [-1, -1], [-2, 2], [1, 3]], [3, 3, 3, 5]),
+        ([[18, 7], [7, 3], [4, 9], [17, 14]], [11, 6, 13, 7])
+    ]
+)
+def test_sides_update(pts, exp):
     """Testing sides"""
-    sides = SideSet()
-    points = PointSet([[0, 0], [0, 2], [0, 3], [0, 1]])
-    sides.update(points)
-    assert sides.side_a == 0
-    assert sides.side_b == 1
-    assert sides.side_c == 0
-    assert sides.side_d == 1
+    qr_code = QRGeometry(pts)
+    assert qr_code.side_a == exp[0] and qr_code.side_b == exp[1] and \
+        qr_code.side_c == exp[2] and qr_code.side_d == exp[3]
 
 # test on pictures with approx
 # https://docs.pytest.org/en/4.6.x/reference.html

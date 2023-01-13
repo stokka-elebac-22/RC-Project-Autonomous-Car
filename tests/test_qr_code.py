82efe6f844a35cd084234feb9d2f6280cdf96890
Asbjørn Stokka <asbjorn.stokka@lyse.net>
2023-01-13 15:55:29 +0100

    Tests

--- tests/test_qr_code.py
+++ tests/test_qr_code.py
@@ -3,6 +3,7 @@ from computer_vision.qr_code.qr_code import PointSet, SideSet
 import pytest
 
 def test_points_update():
+    """Testing points"""
     points = PointSet()
     points.update([0, 1, 2, 3])
     assert points.point0 == 0
@@ -11,6 +12,7 @@ def test_points_update():
     assert points.point3 == 3
 
 def test_sides_update():
+    """Testing sides"""
     sides = SideSet()
     points = PointSet([[0, 0], [0, 2], [0, 3], [0, 1]])
     sides.update(points)

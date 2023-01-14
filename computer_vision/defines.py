ba7fd38417a969908c986c2cf974c9ba62576835
Asbjørn Stokka <asbjorn@maxit-as.com>
2023-01-14 20:34:05 +0100

    fixed message id in defines

--- computer_vision/defines.py
+++ computer_vision/defines.py
@@ -16,18 +16,19 @@ class Position(Enum):
     FRONT_LEFT = 3
 
 class Message_Id(Enum):
-    CAN_MOTOR_DATA_ID = 16       # 0x010
-    CAN_STEERING_DATA_ID = 17    # 0x011
+    CAN_MOTOR_DATA_ID = 16       # 0x010 //  00010 000
+    CAN_STEERING_DATA_ID = 24    # 0x018 //  00011 000
 
-    CAN_SENSOR0_DATA_ID = 32      # 0x020 (+ position)
-    CAN_SENSOR1_DATA_ID = 36      # 0x024 (+ position)
+    CAN_SENSOR0_DATA_ID = 32      # 0x020 // 00100 000 (+ position)
+    CAN_SENSOR1_DATA_ID = 40      # 0x028 // 00110 000 (+ position)
 
     CAN_LIGHT_CONTROL_ID = 64    # 0x040
-    CAN_RGB_CONTROL_ID = 65      # 0x041
+    CAN_RGB_CONTROL_ID = 72      # 0x048
 
     CAN_BUZZER_CONTROL_ID = 128  # 0x080
-    CAN_TEST_MSG_ID = 129        # 0x081
-    CAN_DEVICE_SETTINGS_ID = 130 # 0x082
+
+    CAN_TEST_MSG_ID = 136        # 0x088
+    CAN_DEVICE_SETTINGS_ID = 152 # 0x098
 
 if __name__ == "__main__":
     test = Position(3)

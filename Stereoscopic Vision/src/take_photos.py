9a014647fcda073bc6f90fc52791daec7118c4e0
eirikreiestad <eirik@reiestad.no>
2023-01-17 15:52:57 +0100

    added photos and restructured files

--- Stereoscopic Vision/src/take_photos.py
+++ Stereoscopic Vision/src/take_photos.py
@@ -6,7 +6,7 @@ Assuming you take photos with two camera at the same time.
 It will work with one camera, but it will end up in the calibrate left directory
 """
 
-DIRECTORY = 'Stereoscopic Vision/images/stereo_vision'
+DIRECTORY = 'Stereoscopic Vision/images/depth_calibration'
 
 cam1 = Camera(0, 'left')
 cam2 = Camera(1, 'right')
@@ -27,7 +27,7 @@ while True:
     if cv.waitKey(1) & 0xFF == ord('c'): # capture frame by pressing c
         print('Capturing...')
         for i, frame in enumerate(frames):
-            cv.imwrite(f'{DIRECTORY}_{camera_sides[i]}/{frame[1]}_{count}.jpg', frame[0])
+            cv.imwrite(f'{DIRECTORY}/{camera_sides[i]}/{frame[1]}_{count}.jpg', frame[0])
         count += 1
 
     if cv.waitKey(1) & 0xFF == ord('s'): # stop loop by pressing s

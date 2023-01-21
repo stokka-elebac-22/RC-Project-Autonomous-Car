4c0e07b83cce1e55d61c527d301c68e77f4e8e01
eirikreiestad <eirik@reiestad.no>
2023-01-21 13:20:38 +0100

    made a take camera script that only capture photo when qr code is detected (for testing)

--- computer_vision/qr_code/take_phtos.py
+++ computer_vision/qr_code/take_phtos.py
@@ -6,48 +6,52 @@ It will work with one camera, but it will end up in the calibrate left directory
 """
 
 if __name__ == '__main__':
-    DIRECTORY = 'tests/images/qr_code'
+    DIRECTORY = 'tests/images/qr_code/distance'
     CAMERA_ID_LEFT = 1
     CAMERA_ID_RIGHT = 2
     BOARD_DIMENSIONS = (8, 6)
     cam1 = cv.VideoCapture(CAMERA_ID_LEFT)
     cam2 = cv.VideoCapture(CAMERA_ID_RIGHT)
-    cameras = [(cam1, 'left'), (cam2, 'right')]
+    # cameras = [(cam1, 'left'), (cam2, 'right')]
+    cameras = [(cam1, 'left')]
 
-    count = 0
+    qcd = cv.QRCodeDetector()
+
     trigger = False
+    count = 0
     while True:
+        frames = []
+        rets = True
+        for cam in cameras:
+            ret, frame = cam[0].read()
+            cv.imshow(cam[1], frame)
+            if not ret:
+                rets = False
+                break
+            frames.append((frame, cam[1]))
+
+        if cv.waitKey(1) & 0xFF == ord('s'): # stop loop by pressing s
+            print('Stopping...')
+            break
+
         if cv.waitKey(1) & 0xFF == ord('c'):
             print('Capturing...')
             trigger = True
 
-        if trigger is False:
-            continue
-
-        frames = []
-        rets = True
-        for cam in cameras:
-            ret, frame = cam.read()
-            if not ret:
-                rets = False
-                break
-            gray = cv.cvtColor(cam[0], cv.COLOR_BGR2GRAY)
-            frames.append((frame, cam[1]))
-            ret, _ = cv.findChessboardCorners(gray, BOARD_DIMENSIONS, None)
-            if not ret:
-                rets = False
-                break
-            cv.imshow(cam[1], cam[0])
         if not rets:
             continue
 
-        print('Done\n')
-        for i, frame in enumerate(frames):
-            cv.imwrite(f'{DIRECTORY}/{cameras[i][1]}/{frame[1]}_{count}.jpg', frame[0])
-        trigger = False
-        count += 1
+        for frame in frames:
+            ret, _, _, _ = qcd.detectAndDecodeMulti(frame[0])
+            if not ret:
+                rets = False
+                break
+        if not rets:
+            continue
 
-        if cv.waitKey(1) & 0xFF == ord('s'): # stop loop by pressing s
-            break
-
-
+        if trigger:
+            print('Done\n')
+            for frame, title in frames:
+                cv.imwrite(f'{DIRECTORY}/{title}/{title}_{count}.jpg', frame)
+            count += 1
+            trigger = False

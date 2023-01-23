232fde9a20f3f27ce7d182def345233b2ff4faff
vyjur <idyllistoic@gmail.com>
2023-01-23 14:01:30 +0100

    refactor code

--- computer_vision/line_detection/parking_slot_detection.py
+++ computer_vision/line_detection/parking_slot_detection.py
@@ -8,6 +8,7 @@ from main import LineDetector
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+from qr_code.qr_code import QRCode
 
 class ParkingSlotDetector(LineDetector):
     """

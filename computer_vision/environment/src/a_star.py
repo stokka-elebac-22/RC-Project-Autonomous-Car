80717fc3ee6651adefd5f6d4d65df2c69dcb929d
eirikreiestad <eirik@reiestad.no>
2023-01-26 13:48:39 +0100

    added basic for pathfinding as well as some tests

---
+++ computer_vision/environment/src/a_star.py
@@ -0,0 +1,35 @@
+'''
+A star algorithm
+'''
+import dataclasses
+
+@dataclasses.dataclass
+class Node:
+    '''Node'''
+    def __init__(self, position, parent=None, f_value=None) -> None:
+        self.position = position
+        self.parent: Node = parent
+        self.f_value = f_value
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+class AStar:
+    '''A* Algorithm'''
+    def find_path(self, mat, start_pos, end_pos) -> Node:
+        '''Returns the start node'''
+        # The f_value will be the total distance and calculated with pythagoras without the sqare root
+        f_value = abs(start_pos[0]-end_pos[0])**2 + abs(start_pos[1]-end_pos[1])**2
+        start_node = Node(start_pos, f_value=f_value)
+        valid = ['None'] # a list of object names that are valid
+        open_list = [] # a list of possible candidates to be the next current node
+
+        while open_list:
+            pass
+
+        return start_node
+
+
+    def get_data(self):
+        '''Returns the data'''
---
+++ tests/tests_environment/test_a_star.py
@@ -0,0 +1,59 @@
+'''A star'''
+import pytest
+from computer_vision.environment.src.a_star import Node, AStar
+
+class TestNode:
+    '''Test node'''
+    def test_node(self):
+        '''Test Node'''
+        node = Node((0, 0))
+        assert node.parent is None
+        parent = Node(0, 1)
+        node.parent = parent
+        assert node.parent.position == (0, 1)
+        node_dup = Node((0, 0))
+        assert node == node_dup
+
+class TestAStar:
+    '''Test A*'''
+    @pytest.mark.parametrize(
+        ['param', 'exp'],
+        [
+            ((2, 1), (0, 1),
+            [[
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]],
+            [
+                Node((2, 1), parent=
+                Node((1, 1), parent=
+                Node((0, 1), parent=None)))
+            ]),
+            ((2, 2), (0, 2),
+                [[
+                    [0, 0, 0],
+                    [0, 1, 1],
+                    [0, 0, 0],
+                ]],
+                [
+                    Node((2, 2), parent=
+                    Node((2, 1), parent=
+                    Node((1, 0), parent=
+                    Node((0, 1), parent=
+                    Node((0, 2), parent=None)))))
+                ]
+            )
+        ]
+    )
+    def test_find_path(self, param, exp):
+        '''Test find path'''
+        a_star = AStar()
+        cur_node: Node = a_star.find_path(param[2], param[0], param[1])
+        cur_exp: Node = exp
+
+        assert cur_node == cur_exp
+        while cur_node.parent is not None:
+            cur_node = cur_node.parent
+            cur_exp = cur_exp.parent
+            assert cur_node == cur_exp

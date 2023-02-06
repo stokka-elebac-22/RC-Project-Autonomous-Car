95921f2b03533ff94b90e0c339cc6dfa930bebd2
unknown <idyllistoic@gmail.com>
2023-02-06 20:30:35 +0100

    added derivative vectors and helping functions

--- computer_vision/pathfinding/helping_functions.py
+++ computer_vision/pathfinding/helping_functions.py
@@ -1,6 +1,7 @@
 import math
 
 def get_angle(v):
+    '''Get the angle from -180 to 180 where y-axis is 0'''
     rad_result = math.atan(v[1]/v[0])
     degrees_result = math.degrees(rad_result)
     if v[1] < 0 and v[0] > 0:
@@ -10,4 +11,5 @@ def get_angle(v):
     return degrees_result
 
 def get_abs_velo(v):
+    """Calculate the absolute value of a vector"""
     return math.sqrt(v[0]**2 + v[1]**2)

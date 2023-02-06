5bafee11d6b13a0e12c9bba567a44e06a339b087
vyjur <idyllistoic@gmail.com>
2023-02-06 13:12:54 +0100

    added derivative

--- computer_vision/pathfinding.py
+++ computer_vision/pathfinding.py
@@ -2,6 +2,8 @@
 from typing import TypedDict
 import cv2
 import numpy as np
+import math
+import cv2
 import pygame as pg
 from pygame.locals import QUIT  # pylint: disable=no-name-in-module
 try:
@@ -326,3 +328,83 @@ if __name__ == "__main__":
 
 x_values = [ point[1] for i, point in enumerate(path) if i % 5 == 0 ]
 y_values = [ BOARD_SIZE[0] - (point[0] + 1) for i, point in enumerate(path) if i % 5 == 0]
+
+# Source: https://stackoverflow.com/questions/1251438/catmull-rom-splines-in-python
+def CatmullRomSpline(P0, P1, P2, P3, a, nPoints=100):
+    """
+    P0, P1, P2, and P3 should be (x,y) point pairs that define the Catmull-Rom spline.
+    nPoints is the number of points to include in this curve segment.
+    """
+    # Convert the points to numpy so that we can do array multiplication
+    P0, P1, P2, P3 = map(np.array, [P0, P1, P2, P3])
+
+    # Calculate t0 to t4
+    alpha = a
+    def tj(ti, Pi, Pj):
+        xi, yi = Pi
+        xj, yj = Pj
+        return ( ( (xj-xi)**2 + (yj-yi)**2 )**0.5 )**alpha + ti
+
+    t0 = 0
+    t1 = tj(t0, P0, P1)
+    t2 = tj(t1, P1, P2)
+    t3 = tj(t2, P2, P3)
+
+    # Only calculate points between P1 and P2
+    t = np.linspace(t1,t2,nPoints)
+
+    # Reshape so that we can multiply by the points P0 to P3
+    # and get a point for each value of t.
+    t = t.reshape(len(t),1)
+
+    A1 = (t1-t)/(t1-t0)*P0 + (t-t0)/(t1-t0)*P1
+    A2 = (t2-t)/(t2-t1)*P1 + (t-t1)/(t2-t1)*P2
+    A3 = (t3-t)/(t3-t2)*P2 + (t-t2)/(t3-t2)*P3
+
+    B1 = (t2-t)/(t2-t0)*A1 + (t-t0)/(t2-t0)*A2
+    B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3
+
+    C  = (t2-t)/(t2-t1)*B1 + (t-t1)/(t2-t1)*B2
+    return C
+
+def CatmullRomChain(P,alpha):
+    """
+    Calculate Catmull Rom for a chain of points and return the combined curve.
+    """
+    sz = len(P)
+
+    # The curve C will contain an array of (x,y) points.
+    C = []
+    for i in range(sz-3):
+        c = CatmullRomSpline(P[i], P[i+1], P[i+2], P[i+3],alpha)
+        C.extend(c)
+
+    return C
+
+def derivative(P1, P2):
+    v_x = (P2[0] - P1[0]) / 2
+    v_y = (P2[0] - P1[0]) / 2
+    return (v_x, v_y)
+
+def speed(v):
+    return math.sqrt(v[0]**2 + v[1]**2)
+
+def angle(v):
+    rad_result = math.atan(v[0], v[1])
+    degrees_result = math.degrees(rad_result)
+    
+
+plt.xlim(0, BOARD_SIZE[1])
+plt.ylim(0, BOARD_SIZE[0])
+
+a = 0.
+
+new_path = [(value[1], BOARD_SIZE[0] - (value[0]+1)) for i, value in enumerate(path) if i % 3 == 0]
+
+c = CatmullRomChain(new_path, a)
+x_values, y_values = zip(*c)
+plt.plot(x_values,y_values,c='red')
+
+plt.grid()
+plt.plot(x_values, y_values, marker="o", markersize=2, markeredgecolor="red", markerfacecolor="green")
+plt.show()

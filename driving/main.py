c0f0d55d944dc1863a07bba36c66164e36f2197f
eirikreiestad <eirik@reiestad.no>
2023-02-10 18:16:26 +0100

    start

---
+++ driving/main.py
@@ -0,0 +1,17 @@
+'''
+The main file for the driving logic.
+This file should only contain short code
+'''
+
+# ---------- CONSTANTS ---------- #
+
+# ---------- MAIN ---------- #
+if __name__ == '__main__':
+    # ---------- INIT ---------- #
+    # init camera
+
+    # ---------- LOOP ---------- #
+    while True:
+        # ---------- CHECKS ---------- #
+        # ---------- ACTION ---------- #
+        pass

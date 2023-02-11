566d449987bee73c20699049c8c4c3d7d5494041
eirikreiestad <eirik@reiestad.no>
2023-02-11 22:03:22 +0100

    lint main.py traffic sign detection

--- computer_vision/traffic_sign_detection/main.py
+++ computer_vision/traffic_sign_detection/main.py
@@ -1,43 +1,50 @@
-"""Importing necessary libraries"""
+'''Importing necessary libraries'''
 import cv2
+from typing import TypedDict
 
+SignSize = TypedDict('SignSize', {
+    'px': int,
+    'mm': int,
+    'distance': int,
+})
 
 class TrafficSignDetector:
-    """
+    '''
     DOC:
-    """
-
-    def __init__(self, model='computer_vision/traffic_sign_detection/stop_sign_model.xml', size_px=None, size_mm=None, distance=None):
+    '''
+    def __init__(self,
+            model='computer_vision/traffic_sign_detection/stop_sign_model.xml',
+            size: SignSize=None):
         self.cascade = cv2.CascadeClassifier(model)
-        self.sign_size_px = size_px
-        self.sign_size_mm = size_mm
-        self.sign_distance = distance
+        if size is None:
+            self.size = {}
 
-        if size_px is None:
-            self.qr_size_px = 1
-        if size_mm is None:
-            self.qr_size_mm = 1
-        if distance is None:
-            self.qr_distance = 1
+        if self.size.get('px') is None:
+            self.size['px'] = 1
+        if self.size.get('mm') is None:
+            self.size['mm'] = 1
+        if self.size.get('distance') is None:
+            self.size['distance'] = 1
 
     def detect_signs(self, image):
-        """Detect the signs based on the model used"""
+        '''Detect the signs based on the model used'''
         signs = self.cascade.detectMultiScale(image)
         return signs
 
     def get_distance(self, sign):
-        focal_length = (self.sign_size_px / self.sign_size_mm) * self.qr_distance
-        distance = (self.sign_size_mm * focal_length) / sign[3]
+        '''Get distance'''
+        focal_length = (self.size['px'] / self.size['mm']) * self.size['distance']
+        distance = (self.size['mm']* focal_length) / sign[3]
         return distance
 
     def show_signs(self, image, signs):
-        """Draw the signs on the image"""
+        '''Draw the signs on the image'''
         for (x_coordinate, y_coordinate, width, height) in signs:
             cv2.rectangle(image, (x_coordinate, y_coordinate),
                           (x_coordinate+width, y_coordinate+height), (255, 0, 0), 5)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     img = cv2.imread('computer_vision/traffic_sign_detection/images/test/1.jpg')
 
@@ -47,7 +54,12 @@ if __name__ == "__main__":
     dim = (img_width, img_height)
     img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
-    traffic_sign_detection = TrafficSignDetector(size_mm=61, size_px=10, distance = 200)
+    sign_size: SignSize = {
+        'px': 10,
+        'mm': 61,
+        'distance': 200
+    }
+    traffic_sign_detection = TrafficSignDetector(sign_size)
     output_signs = traffic_sign_detection.detect_signs(img)
     traffic_sign_detection.show_signs(img, output_signs)
 

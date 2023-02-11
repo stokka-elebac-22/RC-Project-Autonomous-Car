107d591c77901f050806b14a17d53498d3c55e3a
eirikreiestad <eirik@reiestad.no>
2023-02-11 18:56:57 +0100

    converted to only single quotes in test_parking_slot_detection

--- tests/test_line_detection.py
+++ tests/test_line_detection.py
@@ -3,8 +3,8 @@ import pytest
 import cv2
 import numpy as np
 from computer_vision.line_detection.line_detector import LineDetector
-PATH = "computer_vision/line_detection/assets/"
 
+PATH = "computer_vision/line_detection/assets/"
 
 class TestParametrized:
     """
--- tests/test_parking_slot_detection.py
+++ tests/test_parking_slot_detection.py
@@ -1,19 +1,19 @@
-"""Importing needed libraries"""
+'''Importing needed libraries'''
 import pytest
 import cv2
 import numpy as np
 from computer_vision.line_detection.parking_slot_detection import ParkingSlotDetector
 from computer_vision.qr_code.qr_code import QRCode
 
-PATH = "computer_vision/line_detection/assets/parking/"
+PATH = 'computer_vision/line_detection/assets/parking/'
 
 class TestParametrized:
-    """
+    '''
     DOC: Testing ParkingSlotDetector class from module line_detection
-    """
+    '''
 
     def get_image(self, source):
-        """Helping function for tests to get a cv2 image from a source file"""
+        '''Helping function for tests to get a cv2 image from a source file'''
         image = cv2.imread(PATH + source)
         return image
 
@@ -27,7 +27,7 @@ class TestParametrized:
              [np.array([123, 232, 234, 100])]]]),
     ])
     def test_cluster_lines(self, lines, expected):
-        """Test cluster_lines method of ParkingSlotDetector"""
+        '''Test cluster_lines method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         clustered_lines, clustered_coords = parking_slot_detector.cluster_lines(
             lines)
@@ -44,7 +44,7 @@ class TestParametrized:
         [[[-1, 100], [1, 200]], [[123, 123, 321, 321], [123, 123, 321, 321]]])
     ])
     def test_filter_lines(self, lines, coords, expected):
-        """Test filter_lines method of ParkingSlotDetector"""
+        '''Test filter_lines method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         parking_slot_detector.filter_lines(lines, coords)
         assert lines == expected[0]
@@ -57,7 +57,7 @@ class TestParametrized:
          [50, 900]),
     ])
     def test_get_min_max_x(self, coordinates, expected):
-        """Test get_min_max_x method of ParkingSlotDetector"""
+        '''Test get_min_max_x method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         min_x, max_x = parking_slot_detector.get_min_max_x(coordinates)
         assert min_x == expected[0]
@@ -72,7 +72,7 @@ class TestParametrized:
          [np.array([200, 100, 300, 400]), np.array([200, 100, 300, 400])])
     ])
     def test_get_closest_line(self, line_coords, points, expected):
-        """Test get_closest_line method of ParkingSlotDetector"""
+        '''Test get_closest_line method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         lines = parking_slot_detector.get_closest_line(
             line_coords, points)
@@ -80,12 +80,12 @@ class TestParametrized:
             assert (line == expected[i]).all()
 
     @pytest.mark.parametrize('img_source', [
-        ("4.png"),
-        ("7.png"),
-        ("8.png")
+        ('4.png'),
+        ('7.png'),
+        ('8.png')
     ])
     def test_detect_parking_lines(self, img_source):
-        """Test parking_lines method of ParkingSlotDetector"""
+        '''Test parking_lines method of ParkingSlotDetector'''
         QR_SIZE_PX = 76
         QR_SIZE_MM = 52
         QR_DISTANCE = 500
@@ -106,7 +106,7 @@ class TestParametrized:
         (100, 300, [-1, 20], np.array([100, -80, 300, -280]))
     ])
     def test_get_line_coordinates_from_parameters(self, min_x, max_x, line_parameters, expected):
-        """Test get_line_coordinates_from_parameters method of ParkingSlotDetector"""
+        '''Test get_line_coordinates_from_parameters method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         line_coordinates = parking_slot_detector.get_line_coordinates_from_parameters(
             min_x, max_x, line_parameters)
@@ -123,7 +123,7 @@ class TestParametrized:
             np.array([100, 200, 200, 300]))
         ])
     def test_get_closing_line_of_two_lines(self, lines, expected):
-        """Test get_closing_line_of_two_lines method of ParkingSlotDetector"""
+        '''Test get_closing_line_of_two_lines method of ParkingSlotDetector'''
         parking_slot_detector = ParkingSlotDetector()
         closing_line = parking_slot_detector.get_closing_line_of_two_lines(lines)
         assert (closing_line == expected).all()

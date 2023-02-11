50bb59cf28e170652688c07172455557243c0d02
eirikreiestad <eirik@reiestad.no>
2023-02-11 21:53:05 +0100

    lint main window ui, men stokka må fikse ta vekk pylint disable og fikse resten av lintingen

--- computer_vision/main_window_ui.py
+++ computer_vision/main_window_ui.py
@@ -32,7 +32,7 @@ class Worker(QObject, ):
         self.cam_handler.refresh_camera_list()
         self.finished.emit()
 
-class Ui(QtWidgets.QMainWindow):
+class Ui(QtWidgets.QMainWindow): # pylint: disable=R0902
     '''Class handling Qt GUI control'''
     def __init__(self, ui_file, connection: tuple[str, int], fullscreen):
         self.connection_details = connection
@@ -51,24 +51,25 @@ class Ui(QtWidgets.QMainWindow):
             self.findChild(QtWidgets.QLabel, 'input_img_2')
         ]
 
-        QR_SIZE_PX = 76
-        QR_SIZE_MM = 52
-        QR_DISTANCE = 500
-        self.qr_code = QRCode(QR_SIZE_PX, QR_SIZE_MM, QR_DISTANCE)
+        size = {
+            'px': 76,
+            'mm': 52,
+            'distance': 500,
+        }
+
+        self.qr_code = QRCode(size)
         self.stop_sign_detector = TrafficSignDetector('stop_sign_model.xml')
 
         self.img_output = self.findChild(QtWidgets.QLabel, 'output_img')
         self.output_text = self.findChild(QtWidgets.QLabel, 'output_lbl')
         self.refresh_webcam_list()
 
-
-
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
         #self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
-#        self.update_plot_data()
+        # self.update_plot_data()
 
         if fullscreen:
             self.showFullScreen()
@@ -79,23 +80,26 @@ class Ui(QtWidgets.QMainWindow):
     def update_image(self, cv_img):
         '''Updates the image_label with a new opencv image'''
         output_frame = cv_img
-        qt_img = self.camera_handler.convert_cv_qt(cv_img, self.img_input[0].width(), self.img_input[0].height())
+        qt_img = self.camera_handler.convert_cv_qt(
+            cv_img, self.img_input[0].width(), self.img_input[0].height())
         current_qr_data = self.qr_code.get_data(cv_img)
         output_data = 'Data: \n'
         # print(current_qr_data)
-        if (current_qr_data['ret']):
+        if current_qr_data['ret']:
             self.qr_code.display(output_frame, current_qr_data, verbose=0)
             for i in range(len(current_qr_data['distances'])):
                 output_data += \
-                    'QR-Code ' + str(i) + '\n' + 'Distance: {:.2f} \n Angle: {:.2f} \n'.format(
-                        current_qr_data['distances'][i], current_qr_data['angles'][i])
+                    f"QR-Code {str(i)} \n \
+                        Distance: {round(current_qr_data['distances'][i])} \n \
+                        Angle: {current_qr_data['angles'][i]} \n"
 
                 output_data += 'Data: ' + current_qr_data['info'][i] + '\n'
 
         current_stop_sign = self.stop_sign_detector.detect_signs(cv_img)
         self.stop_sign_detector.show_signs(output_frame, current_stop_sign)
 
-        output_img = self.camera_handler.convert_cv_qt(cv_img, self.img_output.width(), self.img_output.height())
+        output_img = self.camera_handler.convert_cv_qt(
+            cv_img, self.img_output.width(), self.img_output.height())
 
         # print('Setting new image')
         self.img_input[0].setPixmap(qt_img)
@@ -122,7 +126,7 @@ class Ui(QtWidgets.QMainWindow):
             for camera in self.camera_handler.get_camera_list():
                 cbo.addItem(self.camera_handler.get_camera_string(camera['id']))
                 # create the video capture thread
-        self.thread2 = VideoThread(1)
+        self.thread2 = VideoThread(1) # pylint: disable=W0201
         # connect its signal to the update_image slot
         self.thread2.change_pixmap_signal.connect(self.update_image)
         # start the thread

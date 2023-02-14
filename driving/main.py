6d80b14f6ff01f9e34751c9b9ebd7da7cc58de38
eirikreiestad <eirik@reiestad.no>
2023-02-14 19:34:09 +0100

    added env object list

--- driving/main.py
+++ driving/main.py
@@ -2,8 +2,11 @@
 The main file for the driving logic.
 This file should only contain short code
 '''
+from typing import Tuple, List
 from computer_vision.qr_code.qr_code import QRCode
 from computer_vision.environment.src.environment import Environment
+from computer_vision.camera_handler.camera import Camera
+from computer_vision.camera_handler.camera_handler import CameraHandler
 
 # ---------- CONSTANTS ---------- #
 
@@ -15,22 +18,46 @@ if __name__ == '__main__':
     QR_CODE_SIZE_PX = 76
     QR_CODE_SIZE_MM = 52
     QR_CODE_DISTANCE = 500
-    qr_code = QRCode(QR_CODE_SIZE_PX, QR_CODE_SIZE_MM, QR_CODE_DISTANCE)
+    size = {
+        'px': QR_CODE_SIZE_PX,
+        'mm': QR_CODE_SIZE_MM,
+        'distance': QR_CODE_DISTANCE
+    }
+    qr_code = QRCode(size)
     ### init environment ###
     SIZE = (10, 11)
     WINDOW_WIDTH = 600
     WINDOW_SIZE = (WINDOW_WIDTH* (SIZE[1]/SIZE[0]), WINDOW_WIDTH)
     env= Environment(SIZE, 1, {'object_id': 10})
 
+    camera_handler = CameraHandler()
+    camera_handler.refresh_camera_list()
+    available_cameras = camera_handler.get_camera_list()
+
+    # assuming the first camera is the correct one
+    CAMERA = None
+    if len(available_cameras) != 0:
+        camera_id = available_cameras[0].get('id')
+        # CAMERA = Camera(camera_id)
+
     # ---------- LOOP ---------- #
     while True:
         # ---------- GET CAMERA INFORMATION---------- #
+        # The environment objects is a list of tuples with
+        # an object id, x distance and y distance
+        env_objects: List[Tuple[int, int, int]] = []
+
         ### QR Code ###
         # qr_code.get_data(frame)
+
+        ### Traffic Sign ###
 
         ### Line detection ###
 
         # ---------- UPDATE ENVIRONMENT ---------- #
+        # Insert objects into the environment
+        for obj in env_objects:
+            env.insert((env_objects[1], env_objects[2]),
+                env_objects[0])
 
         # ---------- ACTION ---------- #
-        pass

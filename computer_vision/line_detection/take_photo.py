d458181fe7337972185baa509a2d27ce82eb8ea2
Asbjørn Stokka <asbjorn@maxit-as.com>
2023-03-02 13:30:46 +0100

    Updated resolution

--- computer_vision/line_detection/take_photo.py
+++ computer_vision/line_detection/take_photo.py
@@ -3,15 +3,20 @@ import cv2
 
 if __name__ == '__main__':
     DIRECTORY = 'tests/images/parking_slot_detection_2/'
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cam.set(cv2.CAP_PROP_FPS, 30.0)
+    codec = 0x47504A4D  # MJPG
+    cam.set(cv2.CAP_PROP_FOURCC, codec)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     COUNT = 5
     while True:
-        frames = []
+        #frames = []
         RETS = True
         ret, frame = cam.read()
         cv2.imshow('cam', frame)
-        frames.append((frame, cam))
+        #frames.append((frame, cam))
 
         if cv2.waitKey(1) & 0xFF == ord('s'): # stop loop by pressing s
             print('Stopping...')
@@ -22,6 +27,6 @@ if __name__ == '__main__':
 
         if cv2.waitKey(1) & 0xFF == ord('c'):
             print('Capturing...')
-            for frame, title in frames:
-                cv2.imwrite(f'{DIRECTORY}/{title}_{COUNT}.jpg', frame)
+        #    for frame, title in frames:
+            cv2.imwrite(f'{DIRECTORY}/{"title"}_{COUNT}.jpg', frame)
             COUNT += 1

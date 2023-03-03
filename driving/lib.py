4fa08b8c63737e6bfe2f1830a43be993558e0334
eirikreiestad <eirik@reiestad.no>
2023-03-03 19:21:30 +0100

    changed code to check a list of indexes, because it may not start at 0 and it might be spaces between for some reason

--- driving/lib.py
+++ driving/lib.py
@@ -7,15 +7,14 @@ def get_available_cameras() -> Tuple[bool, List[int]]:
     '''
     Returns the index of the available cameras and
     a bool value, indicating if the list is empty or not'''
-    index = 0
+    possible = [0, 1, 2, 3, 4]
     cameras = []
-    while True:
+    for index in possible:
         cap = cv.VideoCapture(index)
         if not cap.read()[0]:
             break
         cameras.append(index)
         cap.release()
-        index += 1
     if len(cameras) == 0:
         return False, None
     return True, cameras

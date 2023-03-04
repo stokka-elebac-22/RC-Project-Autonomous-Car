c9d1a7b91fb8af0ea0c2b6635144013fdc2c06ec
eirikreiestad <eirik@reiestad.no>
2023-03-04 14:24:15 +0100

    changed some names

--- computer_vision/pathfinding/lib.py
+++ computer_vision/pathfinding/lib.py
@@ -1,5 +1,7 @@
 '''Importing libraries'''
 import math
+from environment.src.display import DisplayEnvironment
+from environment.src.environment import Environment
 
 def get_angle(vec: list) -> float:
     '''Get the angle from -180 to 180 where y-axis is 0'''
@@ -15,3 +17,15 @@ def get_angle(vec: list) -> float:
 def get_abs_velo(vec: list) -> float:
     '''Calculate the absolute value of a vector'''
     return math.sqrt(vec[0]**2 + vec[1]**2)
+
+def update_display(
+          display: DisplayEnvironment,
+          environment: Environment,
+          path) -> DisplayEnvironment:
+    '''Update display if there are new changes'''
+    if display is not None:
+        cur_mat = environment.get_data()
+        display.update(cur_mat)
+        for pos in path[1:-1]:
+            display.insert(pos, 'Path')
+    return display

bd2a85b42034e901387714922d2274d1fc8c5f4d
Eirik Reiestad <47319221+EirikReiestad@users.noreply.github.com>
2023-03-04 12:49:40 +0100

    117 driving logic connecting camera (#119)
    * added camera to the driving logic
    
    * added lib to driving
    
    * moved import
    
    * moved back import
    
    * added try
    
    * cap
    
    * changed function
    
    * try
    
    * back to the other code
    
    * code
    
    * changed code to check a list of indexes, because it may not start at 0 and it might be spaces between for some reason
    
    * nvm
    
    * some os command need to run after startup
    
    * code
    
    * added qr code
    
    * fixed string output
    
    * fixed string output
    
    * added yaml
    
    * changed some names because it make more sense
    
    * typing
    
    * added pathfinding and update environment

--- driving/main.py
+++ driving/main.py
@@ -7,6 +7,7 @@ import sys
 import os
 from lib import get_available_cameras
 import cv2 as cv
+import yaml
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -14,7 +15,9 @@ sys.path.append(parent)
 
 # pylint: disable=C0413
 from computer_vision.qr_code.qr_code import QRCode, QRSize
-
+from computer_vision.environment.src.environment import Environment
+from computer_vision.environment.src.lib import Objects
+from computer_vision.environment.src.a_star import AStar
 
 # ---------- CONSTANTS ---------- #
 
@@ -25,6 +28,10 @@ if __name__ == '__main__':
     # need to run this command to get VideoCapture to work after every restart of pi
     os.system('sudo chmod 777 /dev/video0')
 
+    # open yaml file
+    with open('driving/config.yaml', 'r', encoding='utf8') as file:
+        config = yaml.safe_load(file)
+        
     ret, available_cameras = get_available_cameras()
 
     if not ret:
@@ -34,13 +41,21 @@ if __name__ == '__main__':
     sys.stdout.write(f'Connecting to camera {available_cameras[0]}')
     camera = cv.VideoCapture(available_cameras[0])
 
+    ### init environment ###
+    SIZE = (config['environment']['sizex'], config['environment']['sizey'])
+    WINDOW_WIDTH = config['gui']['window_width']
+    WINDOW_SIZE = (WINDOW_WIDTH* (SIZE[1]/SIZE[0]), WINDOW_WIDTH)
+    env= Environment(SIZE, 1, {'object_id': 10})
+    objects = Objects()
+
     ### init qr code ###
     QR_SIZE: QRSize = {
-        'px': 76,
-        'mm': 52,
-        'distance': 500
+        'px': config['qr_code_size']['px'],
+        'mm': config['qr_code_size']['mm'],
+        'distance': config['qr_code_size']['distance'],
     }
     qr_code = QRCode(QR_SIZE)
+    qr_code_id = objects.get_data('QR').id
 
     ### init environment ###
     # ---------- LOOP ---------- #
@@ -55,14 +70,16 @@ if __name__ == '__main__':
         if not qr_data['ret']:
             continue
 
-        sys.stdout.write(f'\
-        QR Code: \n \
-        Distance: {qr_data["distances"][0]} \n \
-        Angle: {qr_data["angles"][0]} \n\n ')
-
-
-        ### Line detection ###
+        # insert the qr codes to the environment
+        for i in range(len(qr_data['info'])):
+            env.insert(qr_data['distances'][i], qr_code_id)
 
         # ---------- UPDATE ENVIRONMENT ---------- #
+        start_pos_path = env.get_pos(10)
+        end_pos_path = env.get_pos(11)
+        cur_mat = env.get_data()
+
+        # ---------- PATH ---------- #
+        ret, path = AStar().get_data(cur_mat, start_pos_path, end_pos_path)
 
         # ---------- ACTION ---------- #

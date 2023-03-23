ab219a84287503419fb4615c9ff834aa50b4460b
eirikreiestad <eirik@reiestad.no>
2023-03-23 11:41:39 +0100

    removed unneccessary qr code in camera

--- computer_vision/camera_handler/camera.py
+++ computer_vision/camera_handler/camera.py
@@ -9,23 +9,6 @@ class Camera:
         self.window_name = window_name
         self.cap = cv.VideoCapture(self.camera_id)
 
-    def run(self, name=None, resize=1, verbose=1):
-        '''Run'''
-        if not name:
-            self.run_video(verbose)
-        else:
-            self.run_image(name, resize, verbose)
-
-    def run_video(self, verbose=1):
-        '''Run video'''
-        while True:
-            frame = self.read()
-            self.detect_qr_code(frame, verbose=verbose)
-            cv.imshow(self.window_name, frame)
-            if cv.waitKey(self.delay) & 0xFF == ord('q'):
-                break
-        cv.destroyWindow(self.window_name)
-
     def read(self, name=None, resize=1):
         '''Read'''
         if not name:

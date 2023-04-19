a8d33c849f532d44fb81990ca69306cadf4c1586
unknown <idyllistoic@gmail.com>
2023-04-19 18:25:24 +0200

    Moved methods / functions

--- computer_vision/pathfinding/lib.py
+++ computer_vision/pathfinding/lib.py
@@ -1,6 +1,5 @@
 '''Importing libraries'''
 import math
-import numpy as np
 try:
     from computer_vision.environment.src.display import DisplayEnvironment
     from computer_vision.environment.src.environment import Environment
@@ -8,68 +7,9 @@ except ImportError:
     from environment.src.display import DisplayEnvironment
     from environment.src.environment import Environment
 
-def get_angle(point_one: tuple, point_two: tuple):
-    '''Get the angle from -180 to 180 where y-axis is 0'''
-    x_val = point_two[0] - point_one[0]
-    y_val = point_two[1] - point_one[1]
-    if x_val == 0:
-        return 0
-    rad = math.atan(y_val/x_val)
-    degrees = math.degrees(rad)
-
-    if x_val > 0:
-        degrees = 90 + degrees
-    else:
-        degrees = -90 + degrees
-    return degrees
-
-# def get_angle(vec: list) -> float:
-#     '''Get the angle from -180 to 180 where y-axis is 0'''
-#     if vec[0] == 0:
-#         rad_result = 0
-#     else:
-#         rad_result = math.atan(vec[1]/vec[0])
-#     degrees_result = math.degrees(rad_result)
-#     temp = 90 - degrees_result
-#     if vec[0] < 0:
-#         temp =  temp - 180
-#     if temp == -180:
-#         temp = 180
-#     return temp
-
 def get_abs_velo(vec: list) -> float:
     '''Calculate the absolute value of a vector'''
     return math.sqrt(vec[0]**2 + vec[1]**2)
-
-# def angle_and_velocity_from_derivative(derivative) -> Tuple[int, int]:
-#     '''
-#     This function returns a list of an abs velocity and
-#     an angle from the curve and the derivative
-#     '''
-#     abs_velos = []
-#     angles = []
-#     for value in derivative:
-#         abs_velos.append(get_abs_velo(value))
-#         angles.append(get_angle(value))
-#     return abs_velos, angles
-
-def get_angle_diff(angles: float) -> list[list[np.ndarray], list[np.ndarray]]:
-    '''Calculate the change in angle'''
-    current_ang = 0
-    angle_diff = []
-    for next_ang in angles:
-        first_diff = math.dist([current_ang], [next_ang])
-        second_diff = 360-abs(first_diff)
-        minimum_diff = min(abs(first_diff), second_diff)
-        if current_ang > next_ang:
-            if abs(first_diff) == minimum_diff:
-                minimum_diff = minimum_diff*-1
-        else:
-            if abs(second_diff) == minimum_diff:
-                minimum_diff = minimum_diff*-1
-        angle_diff.append(minimum_diff)
-        current_ang = next_ang
-    return angle_diff
 
 def update_display(
           display: DisplayEnvironment,
--- tests/tests_pathfinding/test_pathfinding.py
+++ tests/tests_pathfinding/test_pathfinding.py
@@ -1,5 +1,6 @@
 '''Importing needed libraries'''
 import pytest
+import numpy as np
 from computer_vision.pathfinding.pathfinding import PathFinding
 from computer_vision.environment.src.environment import Environment, ViewPointObject
 from computer_vision.environment.src.a_star import AStar
@@ -39,3 +40,281 @@ class TestParametrized:
 
         assert data['times'] == expected[0]
         assert data['angles'] == expected[1]
+
+    @pytest.mark.parametrize('point_one, point_two, expected', [
+        ((32, 29), (31.5, 28.5625), -48.81),
+        ((32, 29), (31.5, 29.4375), -131.19),
+        ((32, 29), (32.5, 28.5625), 48.81),
+        ((32, 29), (32.5, 29.4375), 131.19),
+        ((31, 28), (31, 28), 0),
+        ((31, 28), (31, 29), 0),
+        ((31, 28), (31, 27), 0),
+        ((31, 28), (30, 28), -90),
+        ((31, 28), (32, 28), 90),
+        ])
+    def test_get_angle(self, point_one, point_two, expected):
+        '''Testing get_angle method'''
+        board_size = (60, 115)
+        env_size = 20
+
+        tension = 0
+        velocity = 10
+
+        view_point_object: ViewPointObject = {
+            'view_point': None,
+            'object_id': 10,
+        }
+        env = Environment(board_size, env_size, view_point_object)
+        a_star = AStar()
+
+        path_finding = PathFinding(
+            env,
+            a_star,
+            tension,
+            velocity
+        )
+        assert path_finding.get_angle(point_one, point_two) == pytest.approx(expected, 0.001)
+
+    @pytest.mark.parametrize('angles, expected', [
+        ([40, 45], [40, 5]),
+        ([40, 35], [40, -5]),
+        ([20, 30, 35], [20, 10, 5]),
+        ([175, -175], [175, 10]),
+        ([-175, 175], [-175, -10])
+    ])
+    def test_get_angle_diff(self, angles, expected):
+        '''Get the list of change in angle'''
+        board_size = (60, 115)
+        env_size = 20
+
+        tension = 0
+        velocity = 10
+
+        view_point_object: ViewPointObject = {
+            'view_point': None,
+            'object_id': 10,
+        }
+        env = Environment(board_size, env_size, view_point_object)
+        a_star = AStar()
+
+        path_finding = PathFinding(
+            env,
+            a_star,
+            tension,
+            velocity
+        )
+        assert path_finding.get_angle_diff(angles) == expected
+
+    @pytest.mark.parametrize('param, expected', [
+        ([1, (43, 12), (12, 30), 0.5], 6.987),
+        ([3, (23, 100), (43, 21), 0.2], 5.411),
+        ([3, (2, 12), (32, 56), 0.5], 10.298),
+        ([4, (3, 49), (45, 76), 0.3], 7.232),
+    ])
+    def test_catmull_t_j(self, param, expected):
+        '''Testing t_j method'''
+        board_size = (60, 115)
+        env_size = 20
+
+        tension = 0
+        velocity = 10
+
+        view_point_object: ViewPointObject = {
+            'view_point': None,
+            'object_id': 10,
+        }
+        env = Environment(board_size, env_size, view_point_object)
+        a_star = AStar()
+
+        path_finding = PathFinding(
+            env,
+            a_star,
+            tension,
+            velocity
+        )
+        assert path_finding.catmull_t_j(param[0], param[1], param[2], param[3]
+                   ) == pytest.approx(expected, 0.001)
+
+    @pytest.mark.parametrize('param, expected', [
+        ([(3, 4), (4, 4), (5, 3), (6, 3), 0.5],
+         [
+            np.array([[4.,         4.],
+                      [4.25963561, 3.80092628],
+                      [4.5,        3.5],
+                      [4.74036439, 3.19907372],
+                      [5.,         3.]],
+                     ),
+            np.array([[0.92732365, - 0.38411003],
+                      [0.83009301, - 0.89799471],
+                      [0.7976828, - 1.06928961],
+                      [0.83009301, - 0.89799471],
+                      [0.92732365, - 0.38411003]],
+                     )]),
+        ([(3, 4), (4, 4), (5, 5), (6, 5), 0.5],
+         [
+            np.array([[4.,         4.],
+                      [4.25963561, 4.19907372],
+                      [4.5,        4.5],
+                      [4.74036439, 4.80092628],
+                      [5.,         5.]],
+                     ),
+            np.array([[0.92732365, 0.38411003],
+                      [0.83009301, 0.89799471],
+                      [0.7976828, 1.06928961],
+                      [0.83009301, 0.89799471],
+                      [0.92732365, 0.38411003]],
+                     )]),
+        ([(10, 2), (9, 2), (8, 3), (7, 3), 0.5],
+         [
+            np.array([[9.,         2.],
+                      [8.74036439, 2.19907372],
+                      [8.5,        2.5],
+                      [8.25963561, 2.80092628],
+                      [8.,         3.]],
+                     ),
+            np.array([[-0.92732365,  0.38411003],
+                      [-0.83009301, 0.89799471],
+                      [-0.7976828,   1.06928961],
+                      [-0.83009301,  0.89799471],
+                      [-0.92732365,  0.38411003]],
+                     )]),
+        ([(10, 7), (9, 7), (8, 6), (7, 6), 0.5], [
+            np.array([[9., 7.],
+                     [8.74036439, 6.80092628],
+                      [8.5, 6.5],
+                      [8.25963561, 6.19907372],
+                      [8., 6.]],
+                     ),
+            np.array([[-0.92732365, - 0.38411003],
+                      [-0.83009301, - 0.89799471],
+                      [-0.7976828, - 1.06928961],
+                      [-0.83009301, - 0.89799471],
+                      [-0.92732365, - 0.38411003]],
+                     )]),
+    ])
+    def test_catmull_rom_spline(self, param, expected):
+        '''Testing catmull_rom_spline method'''
+        board_size = (60, 115)
+        env_size = 20
+
+        tension = 0
+        velocity = 10
+
+        view_point_object: ViewPointObject = {
+            'view_point': None,
+            'object_id': 10,
+        }
+        env = Environment(board_size, env_size, view_point_object)
+        a_star = AStar()
+
+        path_finding = PathFinding(
+            env,
+            a_star,
+            tension,
+            velocity
+        )
+        curve, velocity = path_finding.catmull_rom_spline(
+            param[0], param[1], param[2], param[3], param[4], 5)
+        for i, value in enumerate(curve):
+            assert value[0] == pytest.approx(expected[0][i][0], 0.001)
+            assert value[1] == pytest.approx(expected[0][i][1], 0.001)
+        for i, value in enumerate(velocity):
+            assert value[0] == pytest.approx(expected[1][i][0], 0.001)
+            assert value[1] == pytest.approx(expected[1][i][1], 0.001)
+
+    @pytest.mark.parametrize('param, expected', [
+        ([[(3, 4), (4, 4), (5, 3), (6, 3)], 0.5],
+         [
+            np.array([[4.,         4.],
+                      [4.25963561, 3.80092628],
+                      [4.5,        3.5],
+                      [4.74036439, 3.19907372],
+                      [5.,         3.]],
+                     ),
+            np.array([[0.92732365, - 0.38411003],
+                      [0.83009301, - 0.89799471],
+                      [0.7976828, - 1.06928961],
+                      [0.83009301, - 0.89799471],
+                      [0.92732365, - 0.38411003]],
+                     )]),
+        ([[(3, 4), (4, 4), (5, 5), (6, 5)], 0.5],
+         [
+            np.array([[4.,         4.],
+                      [4.25963561, 4.19907372],
+                      [4.5,        4.5],
+                      [4.74036439, 4.80092628],
+                      [5.,         5.]],
+                     ),
+            np.array([[0.92732365, 0.38411003],
+                      [0.83009301, 0.89799471],
+                      [0.7976828, 1.06928961],
+                      [0.83009301, 0.89799471],
+                      [0.92732365, 0.38411003]],
+                     )]),
+        ([[(10, 2), (9, 2), (8, 3), (7, 3)], 0.5],
+         [
+            np.array([[9.,         2.],
+                      [8.74036439, 2.19907372],
+                      [8.5,        2.5],
+                      [8.25963561, 2.80092628],
+                      [8.,         3.]],
+                     ),
+            np.array([[-0.92732365,  0.38411003],
+                      [-0.83009301, 0.89799471],
+                      [-0.7976828,   1.06928961],
+                      [-0.83009301,  0.89799471],
+                      [-0.92732365,  0.38411003]],
+                     )]),
+        ([[(10, 7), (9, 7), (8, 6), (7, 6), [2, 3]], 0.5], [
+            np.array([[9., 7.],
+                     [8.74036439, 6.80092628],
+                      [8.5, 6.5],
+                      [8.25963561, 6.19907372],
+                      [8., 6.],
+                      [8., 6.],
+                      [7.77491679, 5.96303889],
+                      [7.54827569, 5.99746456],
+                      [7.29749674, 6.03315795],
+                      [7., 6.]],
+                     ),
+            np.array([[-0.92732365, - 0.38411003],
+                      [-0.83009301, - 0.89799471],
+                      [-0.7976828, - 1.06928961],
+                      [-0.83009301, - 0.89799471],
+                      [-0.92732365, - 0.38411003],
+                      [-0.92732365, -0.38411003],
+                      [-0.88839533,  0.04167515],
+                      [-0.9397868 ,  0.18698414],
+                      [-1.08149807,  0.05181691],
+                      [-1.31352914, -0.36382651]],
+                     )]),
+    ])
+    def test_catmull_rom_chain(self, param, expected):
+        '''Testing catmull_rom_spline method'''
+        board_size = (60, 115)
+        env_size = 20
+
+        tension = 0
+        velocity = 10
+
+        view_point_object: ViewPointObject = {
+            'view_point': None,
+            'object_id': 10,
+        }
+        env = Environment(board_size, env_size, view_point_object)
+        a_star = AStar()
+
+        path_finding = PathFinding(
+            env,
+            a_star,
+            tension,
+            velocity
+        )
+        curve, velocity = path_finding.catmull_rom_chain(
+            param[0], param[1], 5)
+        for i, value in enumerate(curve):
+            assert value[0] == pytest.approx(expected[0][i][0], 0.001)
+            assert value[1] == pytest.approx(expected[0][i][1], 0.001)
+        for i, value in enumerate(velocity):
+            assert value[0] == pytest.approx(expected[1][i][0], 0.001)
+            assert value[1] == pytest.approx(expected[1][i][1], 0.001)

a6478c9188499f445ce32c6db1c77b65f4435353
Asbjørn Stokka <asbjorn@maxit-as.com>
2023-05-13 12:05:55 +0200

    improved stopping

--- computer_vision/states/stopping.py
+++ computer_vision/states/stopping.py
@@ -10,10 +10,25 @@ class StopSignAction():
         '''Take frame input and output relevant data'''
         current_stop_sign = self.stop_sign_detector.detect_signs(input_data)
         if len(current_stop_sign) > 0:
-            print(current_stop_sign)
+            distance = self.stop_sign_detector.get_distance(current_stop_sign[0]) * 1000
+            if distance < 4.5:
+                print("STOPPING")
+                return len(current_stop_sign), {
+                    "dir_0" : 0,
+                    "dir_1" : 0,
+                    "speed_0" : 0,
+                    "speed_1" : 0
+                }
+            print(f"Distance: {distance}, sign: {current_stop_sign}")
             return len(current_stop_sign), {
                 "dir_0" : 0,
                 "dir_1" : 0,
-                "speed_0" : 0,
-                "speed_1" : 0
+                "speed_0" : 10,
+                "speed_1" : 10
+        }
+        return 0, {
+                "dir_0" : 0,
+                "dir_1" : 0,
+                "speed_0" : 10,
+                "speed_1" : 10
         }
